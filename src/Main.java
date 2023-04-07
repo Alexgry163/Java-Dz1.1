4893abfe2d935269b49e21d98088c1183d590b20
Aleksey Lipin <nat4eg95@gmail.com>
2023-04-07 18:09:37 +0400

    Initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 15600;
+        int bonus = 20;
+
+        System.out.println("Количество начисленных миль за купленный билет:" ticket/bonus);
+
+    }
+}
