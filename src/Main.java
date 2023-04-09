1dd0109294a942a5534211cd8aca0627b96f29bb
Alexgry163 <127955264+Alexgry163@users.noreply.github.com>
2023-04-09 15:19:59 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 15600;
         int bonus = 20;
+        int x = ticket/bonus;
 
-        System.out.println("Количество начисленных миль за купленный билет:" ticket/bonus);
+        System.out.println("Количество начисленных миль за купленный билет:" +x);
 
     }
 }
